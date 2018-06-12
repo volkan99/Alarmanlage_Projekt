d2947ee91c440ee48c09df275f1fc8f9cff41801
volkan99 <39738856+volkan99@users.noreply.github.com>
2018-06-12 19:15:54 +0200

    Led steuern via Python

---
+++ LED mit Python.py
@@ -0,0 +1,27 @@
+import serial
+
+arduino = serial.Serial('/dev/cu.usbmodem1411', 9600)
+
+def An_Aus_Funktion():
+    eingabe = input("Soll LED AN oder AUS sein?")
+
+    if eingabe == AN:
+        print("LED ist AN")
+        arduino.write('1')
+
+        An_Aus_Funktion()
+
+    elif eingabe == AUS:
+        print("LED ist AUS")
+        arduino.write('0')
+
+        An_Aus_Funktion()
+
+    else:
+        print("SRY. New try")
+
+        An_Aus_Funktion()
+
+
+
+An_Aus_Funktion()
